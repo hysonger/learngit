0821ac36a82d1a5e15ea774e292014388cb00c09
HySonger <hysonger@hotmail.com>
2019-01-13 11:05:35 +0800

    Create int.cpp

---
+++ int.cpp
@@ -0,0 +1,15 @@
+/* This is a special test file.
+It is only for testing, you know, it's just a test!
+How can a test evnetually solve the real problem?
+hahaha!!! */
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	cout<<"The number is:"<<4+5<<endl;
+	int d = 4 + 5 + 2 + 56;
+	
+
+	return 0;
+}
